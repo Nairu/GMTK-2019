99606aee30ba6342d7bc213f39f981404fedac6e
nairu <Carniall@googlemail.com>
2019-08-03 14:01:01 +0100

    Adding in further logic

--- GameOverl.gd
+++ GameOverl.gd
@@ -4,7 +4,7 @@ export (String, FILE, "*.tscn") var game_scene
 
 func _ready():
 	$Label.text = "SCORE: " + str(Globals.score) + "\n\nPRESS SPACE TO\nCONTINUE"
-	
+	   
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		Globals.score = 0
