3727549aadd9cc0721b57214896e7812109f72b1
Joe Taylor <CoderJet@gmail.com>
2019-08-03 21:49:16 +0100

    For Niall

--- Scripts/Powerup.gd
+++ Scripts/Powerup.gd
@@ -4,9 +4,9 @@ export var life_duration = 5
 var current_life = 0
 
 # Called when the node enters the scene tree for the first time.
-func _ready():	
-	print("Test")
-#	pass # Replace with function body.
+func _ready():
+	self.connect("area_entered", self, "_on_powerup_body_entered") 
+	#pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -14,3 +14,9 @@ func _process(delta):
 	if current_life >= life_duration:
 		queue_free()	
 	#pass
+
+func _on_powerup_body_entered(body):	
+	if body.name == "Player":
+		emit_signal("powerup_trail_pickup")
+		queue_free()
+	pass
