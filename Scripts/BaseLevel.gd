cc40706b2f4b129790478efd367a56f00c574d76
nairu <Carniall@googlemail.com>
2019-08-04 17:44:55 +0100

    Fixing merge issues

--- Scripts/BaseLevel.gd
+++ Scripts/BaseLevel.gd
@@ -82,37 +82,38 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
 
-func _on_enemy_hit(position):
-	var label_instance = pop_label.instance()
-	label_instance.position = position
-	label_instance.text = str(base_points * combo)
-	Globals.score = (Globals.score + base_points * combo)
-	combo = combo + 1
-	add_child(label_instance)
-	ui_instance.set_score(Globals.score)
-	
-	var powerup =  powerup_trail.instance()
-	
-	var perc = randf()
-	var powerup_type = null
-	
-	if perc <= 1:
-		perc = int(rand_range(0, 4))
+func _on_enemy_hit(position, reward_player):
+	if reward_player:
+		var label_instance = pop_label.instance()
+		label_instance.position = position
+		label_instance.text = str(base_points * combo)
+		Globals.score = (Globals.score + base_points * combo)
+		combo = combo + 1
+		add_child(label_instance)
+		ui_instance.set_score(Globals.score)
 		
-		if perc == 0:
-			powerup_type = powerup.Powerup_Type.SHIELD
-		elif perc == 1:
-			powerup_type = powerup.Powerup_Type.SPREAD
-		elif perc == 2:
-			powerup_type = powerup.Powerup_Type.CROSS
-		elif perc == 3:
-			powerup_type = powerup.Powerup_Type.BOMB
+		var powerup =  powerup_trail.instance()
+		
+		var perc = randf()
+		var powerup_type = null
+		
+		if perc <= 1:
+			perc = int(rand_range(0, 4))
+			
+			if perc == 0:
+				powerup_type = powerup.Powerup_Type.SHIELD
+			elif perc == 1:
+				powerup_type = powerup.Powerup_Type.SPREAD
+			elif perc == 2:
+				powerup_type = powerup.Powerup_Type.CROSS
+			elif perc == 3:
+				powerup_type = powerup.Powerup_Type.BOMB
 				
-		powerup.prepare(powerup_type)
-		powerup.player = player_node
-		powerup.position = position
-		powerup.connect("powerup_pickup", self, "_on_powerup_pickup")
-		add_child(powerup)
+			powerup.prepare(powerup_type)
+			powerup.player = player_node
+			powerup.position = position
+			powerup.connect("powerup_pickup", self, "_on_powerup_pickup")
+			add_child(powerup)
 
 func _on_timer_enemy_timeout():
 	var enemy_instance
