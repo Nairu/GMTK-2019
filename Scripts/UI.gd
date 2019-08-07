ef08c92f02aef2c61a6ea21db03194d729a14072
Joe Taylor <CoderJet@gmail.com>
2019-08-07 20:01:07 +0100

    Committing before pulling

--- Scripts/UI.gd
+++ Scripts/UI.gd
@@ -1,10 +1,19 @@
 extends Control
-	
+
+func _ready():
+	$ShieldCharger.connect("value_changed", self, "_on_update_progress")
+
 func _process(delta):
 	$Score.text = "Score: " + str(Globals.score)
 	$Shield.text = "Shield Health: " + str(Globals.shield)
 	
-	#if Globals.recharge_shield:
-		# 270
-		#print("Charge!")
-		#$ShieldCharge.
+	if Globals.recharge_shield:
+		print("charing")
+		$ShieldCharger.value = 0
+		
+	if $ShieldCharger.value < 5:
+		pass
+		
+func _on_update_progress(value):
+	pass
+	
